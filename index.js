7fbd8886ac99118f3865da8627d77084d45d6379
Milan <milancakic89@gmail.com>
2022-07-15 13:19:17 +0200

    feat: obj to fn

--- index.js
+++ index.js
@@ -4,79 +4,83 @@
  */
 
 
-const SimpleNetworkDetection = {
-    online: true,
-    checking: false,
-    interval: null,
-    observers: [],
-    subscribe: function (fn) {
-        if (!this.observers.length) {
-            this.start();
-        }
-        this.observers.push(fn);
-        return fn;
-    },
-    unsubscribe: function (fn) {
-        this.observers = this.observers.filter(f => f !== fn);
-    },
-    seconds: function() {
-        let seconds = 4;
+function SimpleNetworkDetection(){
+    this.online = true;
+    this.checking = false;
+    this.interval = null;
+    this.observers = [];
+};
 
-        function setSeconds(seconds){
-            if(typeof seconds !== 'number'){
-                throw new Error('Seconds must be number')
-            }
-        }
-
-        function getSeconds(){
-            return seconds;
-        }
-
-        return {
-            set: setSeconds,
-            get: getSeconds
-        }
-
-    }(),
-
-    fire: function () {
-        if (this.observers.length) {
-            this.observers.forEach(fn => {
-                return fn(this.online);
-            })
-        } else {
-            clearInterval(this.interval)
-        }
-
-    },
-    check: function () {
-        if (!this.checking) {
-            this.online = false;
-            this.checking = true;
-            fetch('https://jsonplaceholder.typicode.com/posts/1')
-                .then(res => {
-                    this.checking = false;
-                    this.online = true;
-                    this.fire();
-                })
-                .catch(err => {
-                    this.online = false;
-                    this.checking = false;
-                    this.fire();
-                })
-        }
-    },
-    start: function () {
-        if (this.interval) {
-            clearInterval(this.interval)
-        }
-        this.interval = setInterval(() => {
-            this.check();
-        }, this.seconds.get())
+SimpleNetworkDetection.prototype.fire = function () {
+    if (this.observers.length) {
+        this.observers.forEach(fn => {
+            return fn(this.online);
+        })
+    } else {
+        clearInterval(this.interval)
     }
 }
 
-SimpleNetworkDetection.seconds.set(4)
-SimpleNetworkDetection.start();
 
-module.exports.NetworkDetection = SimpleNetworkDetection;
+SimpleNetworkDetection.prototype.check = function () {
+    if (!this.checking) {
+        this.online = false;
+        this.checking = true;
+        fetch('https://jsonplaceholder.typicode.com/posts/1')
+            .then(res => {
+                this.checking = false;
+                this.online = true;
+                this.fire();
+            })
+            .catch(err => {
+                this.online = false;
+                this.checking = false;
+                this.fire();
+            })
+    }
+}
+
+SimpleNetworkDetection.prototype.start = function () {
+    if (this.interval) {
+        clearInterval(this.interval)
+    }
+    this.interval = setInterval(() => {
+        check();
+    }, this.seconds.get() * 1000)
+}
+
+SimpleNetworkDetection.prototype.unsubscribe = function () {
+    this.observers = this.observers.filter(f => f !== fn);
+}
+
+SimpleNetworkDetection.prototype.subscribe = function (fn) {
+    if (this.observers.length) {
+        this.start();
+    }
+    this.observers.push(fn);
+    return fn;
+}
+
+SimpleNetworkDetection.prototype.seconds = (function () {
+    let seconds = 4;
+
+    function setSeconds(sec) {
+        if (typeof sec !== 'number') {
+            throw new Error('Seconds must be number')
+        }
+        seconds = sec;
+    }
+
+    function getSeconds() {
+        return seconds;
+    }
+
+    return {
+        set: setSeconds,
+        get: getSeconds
+    }
+})();
+
+const NetworkDetection = new SimpleNetworkDetection();
+
+module.exports.NetworkDetection = NetworkDetection;
